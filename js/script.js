feb3defd73598352ff6a1f55c8950bd6733ff0e6
Diana Duong <dduong013@gmail.com>
2016-06-08 23:24:42 -0500

    updated

--- js/script.js
+++ js/script.js
@@ -1,5 +1,23 @@
+//myApp app module
 angular.module('myApp', [])
-	.controller('MultiplicationCtrl', function() {
+	//declaring a controller called MultiplicationCtrl and defining it as vm. 
+	// the $attrs is used to pull in an initial value for the table size from your template
+	// the $attr service provides a map of all the attributes present on the element which the ng-controller was placed on
+	.controller('MultiplicationCtrl', function($attrs) {
 		var vm = this;
-		vm.numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+		//a controller variable defined as vm.numberLimit that uses the $attr service
+		//if the attribute is not set then the default is 10
+		vm.numberLimit = $attrs.initialNumberLimit || 10;
+		// vm.numbers is another defined controller variable as a function that returns an array of numbers 0 to the provided limit
+		vm.numbers = function() {
+			var numbers = [];
+			for(var i=0; i < vm.numberLimit; i++) {
+			    numbers[i] = i + 1;
+			}
+			return numbers;
+        }
+        //function to multiply a with b
+        vm.compute = function(a,b) {
+        	return a * b;
+        }
 	})
